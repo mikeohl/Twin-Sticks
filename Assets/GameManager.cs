69e015e9c69d08e5cfcf6605121e165b1a776f27
mikeohl <mikeohl@live.com>
2018-02-04 01:36:19 -0800

    Script for managing game / replay of objects

---
+++ Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+    public bool isRecording = false;
+    //private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        //player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            isRecording = false;
+            //Playback();
+        } else
+        {
+            isRecording = true;
+            //Record();
+        }
+	}
+
+    //void Record()
+    //{
+    //    ReplaySystem buffer = player.GetComponent<ReplaySystem>();
+    //    buffer.Record();
+    //}
+
+    //void Playback ()
+    //{
+    //    ReplaySystem buffer = player.GetComponent<ReplaySystem>();
+    //    buffer.PlayBack();
+    //}
+}
