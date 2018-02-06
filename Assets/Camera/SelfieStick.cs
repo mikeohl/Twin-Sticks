9249803a1e1ecc7efb2a40f89705f93f9124f598
mikeohl <mikeohl@live.com>
2018-02-06 03:06:09 -0800

    Script to control orientation and distance of camera to player

---
+++ Assets/Camera/SelfieStick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SelfieStick : MonoBehaviour {
+
+    public Player player;
+    public float panSpeed = 1f;
+    private Vector3 armRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindObjectOfType<Player>();
+        armRotation = transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        armRotation.y += CrossPlatformInputManager.GetAxis("Horizontal2") * panSpeed;
+        armRotation.x += CrossPlatformInputManager.GetAxis("Vertical2") * panSpeed;
+        transform.position = player.transform.position;
+        transform.rotation = Quaternion.Euler(armRotation);
+    }
+}
