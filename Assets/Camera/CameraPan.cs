b8ac1fc29e0d5a2cb0b957a5167ceff2c7ad9971
mikeohl <mikeohl@live.com>
2018-02-06 03:05:47 -0800

    Script to control camera pan speed and look at player

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraPan : MonoBehaviour {
+
+    public Player player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindObjectOfType<Player>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        //Debug.Log("Horizontal2 " + CrossPlatformInputManager.GetAxis("Horizontal2"));
+        //transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        transform.LookAt(player.transform);
+	}
+}
